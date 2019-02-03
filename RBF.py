01fbf6c6531859093f3aad6ca9b6ed7870c2d055
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2019-02-03 21:42:10 +0330

    moved execute part to execute def

--- RBF.py
+++ RBF.py
@@ -1,15 +1,27 @@
 import sys
-inp_file = sys.argv[1]
 commands = [  "[" , "]" , "<" , ">" , "+" , "-" , "." , "," ]
-try:
-    inp = open(inp_file,"r")
-except:
-    print("fatal error, file not found")
-    exit()
 
-m = inp.read()
-c = []
-for i in range(len(m)
-):
-    if m[i] in commands : c.append(m[i])
-print(c)
+
+
+
+def execute(inp_file):
+    try:
+        inp = open(inp_file,"r")
+    except:
+        print("fatal error, file not found")
+        exit()
+
+    m = inp.read()
+    c = []
+    for i in range(len(m)):
+        if m[i] in commands : c.append(m[i])
+    print(c)
+
+
+
+def main():
+    if len(sys.argv) == 2: execute(sys.argv[1])
+    else: print("Usage:", sys.argv[0], "filename")
+
+
+main()
